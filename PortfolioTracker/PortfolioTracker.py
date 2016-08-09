53834a7d054a38801dcb3a8fe023de17e1cd541f
Aditya Kharosekar <adityakharosekar@gmail.com>
2016-08-09 17:47:39 +0530

    Mainly addition of some comments

--- PortfolioTracker/PortfolioTracker.py
+++ PortfolioTracker/PortfolioTracker.py
@@ -6,17 +6,21 @@ schema_filename = "Database_schema.sql" #contains the schema for the database.
 
 db_is_new = not os.path.exists(db_filename) #False if db already exists. True if it does not
 
-#prints options and returns index of action chosen
+#This function prints the basic options available to the suer. It prints these options and returns index of action chosen
 def printOptions():
     print("What do you want to do?")
-    print("1. Add stocks to your portfolio")
-    print("2. Remove stocks from your portfolio")
-    print("3. Search your portfolio for stock-specific information")
-    print("4. View your portfolio")
+    print("1. Add stocks to your portfolio")        #i.e. enter a purchase of some stocks
+    print("2. Remove stocks from your portfolio")   #i.e. enter a sale of some stocks
+    print("3. View your activity for a specific stock") #if user is interested in seeing their activity and current holdings in a particular stock
+    print("4. View your entire portfolio")
     print("5. Exit")
     x=int(input())
     return x
 
+# NOTE: If user buys the same stock at two different price points, the database stores this as two different transactions.
+# Additions/Modifications - 
+#   1. After inputting all information, show user summary of transaction and ask to confirm
+#   2. Validation for purchase date
 def processAdd():
     conn=sqlite3.connect(db_filename)
     cursor = conn.cursor()
@@ -31,14 +35,7 @@ def processAdd():
 
     cursor.execute("""INSERT INTO UserInfo(stockSymbol, dateofPurchase, avgPurchasePrice, numOfStocks) VALUES(?,?,?,?)""", (symbol, date, price, num,))
     conn.commit()
-    print("Transaction successfully completed")
-    print("The current status of your portfolio: ")
-
-    cursor.execute("""SELECT * FROM UserInfo""")
-    print("Company           Date of Purchase           Average Purchase Price           Number of stocks held")
-    for row in cursor:
-        print("{0}           {1}           {2}           {3}".format(row[0], row[1], row[2], row[3]))
-
+    print("Transaction successfully entered")
     conn.close()
 
 
@@ -67,11 +64,12 @@ def checkIfEmpty():
     conn.close()
     return flag
 
+#This function will create a database which holds the user's activity if this database does not already exist
 def createSQLiteDB():
     conn = sqlite3.connect(db_filename)
     cursor = conn.cursor()
-    if db_is_new:                           # I need to create a schema only if database doesn't already exist. If there already is a database,
-                                            # creating the schema again will be an error
+    if db_is_new:                           #I need to execute the schema only if database doesn't already exist. If there already is a database,
+                                            #executing the schema again will be an error
         with open(schema_filename) as f:
             schema = f.read()               
             conn.executescript(schema)     
@@ -81,7 +79,7 @@ def createSQLiteDB():
 def main():
     createSQLiteDB()
     selection=printOptions()
-    while (selection!=5):
+    while (selection!=5): #while the user wants to stay in the program
         if (selection==1):
             processAdd()
         elif (selection==2):
